b8a9e3cf619f1601a3bfd5b3fd824415534e4cf9
xiaye1 <1550469654@qq.com>
2016-11-28 15:31:11 +0800

    xiaye1

--- BusSearch/app/src/main/java/com/search/bus/bussearch/MainActivity.java
+++ BusSearch/app/src/main/java/com/search/bus/bussearch/MainActivity.java
@@ -4,15 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    //温阳提交的注释
-    //liyue
-    //LUN
-    //yuan
-    //test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-      //push Liye1
     }
 }
