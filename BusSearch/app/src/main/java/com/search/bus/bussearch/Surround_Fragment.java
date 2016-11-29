0903afa706ba7344bcc20d69e0567ee121f8aa84
liyue1 <842414936@qq.com>
2016-11-29 16:29:51 +0800

    完善代码

--- BusSearch/app/src/main/java/com/search/bus/bussearch/Surround_Fragment.java
+++ BusSearch/app/src/main/java/com/search/bus/bussearch/Surround_Fragment.java
@@ -12,8 +12,9 @@ import com.amap.api.maps2d.AMap;
 import com.amap.api.maps2d.MapView;
 
 /**
- * 作者 夏晔
- *  2016/11/29.
+ * 作者 夏晔         修改者：李越
+ *  2016/11/29.      2016.11.29
+ *  编写             添加地图控件
  */
 public class Surround_Fragment extends Fragment {
 
@@ -25,11 +26,55 @@ public class Surround_Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View view=inflater.inflate(R.layout.surround_fragment, container, false);
-        mapView = (MapView)view.findViewById(R.id.map);
+        mapView = (MapView)view.findViewById(R.id.map);//获取控件
         mapView.onCreate(savedInstanceState);// 此方法必须重写
         if (aMap == null) {
             aMap = mapView.getMap();
         }
         return view;
+    }
+
+    /**
+     * 作者：李越
+     * 2016.11.29
+     * 方法必须重写
+     */
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    /**
+     * 作者：李越
+     * 2016.11.29
+     * 方法必须重写
+     */
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    /**
+     * 作者：李越
+     * 2016.11.29
+     * 方法必须重写
+     */
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    /**
+     * 作者:李越
+     * 2016.11.29
+     * 方法必须重写
+     */
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
     }
 }
