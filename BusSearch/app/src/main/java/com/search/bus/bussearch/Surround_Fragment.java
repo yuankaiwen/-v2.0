9c73b8d4776d16a46ec4625e74224a06aed32d14
liyue1 <842414936@qq.com>
2016-11-29 16:18:50 +0800

    make map show

--- BusSearch/app/src/main/java/com/search/bus/bussearch/Surround_Fragment.java
+++ BusSearch/app/src/main/java/com/search/bus/bussearch/Surround_Fragment.java
@@ -3,9 +3,13 @@ package com.search.bus.bussearch;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.amap.api.maps2d.AMap;
+import com.amap.api.maps2d.MapView;
 
 /**
  * 作者 夏晔
@@ -14,10 +18,18 @@ import android.view.ViewGroup;
 public class Surround_Fragment extends Fragment {
 
     private FragmentManager fm;
+    private MapView mapView;
+    private AMap aMap;
 
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.surround_fragment, container, false);
-    }
 
+        View view=inflater.inflate(R.layout.surround_fragment, container, false);
+        mapView = (MapView)view.findViewById(R.id.map);
+        mapView.onCreate(savedInstanceState);// 此方法必须重写
+        if (aMap == null) {
+            aMap = mapView.getMap();
+        }
+        return view;
+    }
 }
