e997f2b16d4ceee09f7919b633b9da510fb40187
lun <659366659@qq.com>
2016-11-30 10:29:11 +0800

    //LUN1

--- BusSearch/app/src/main/java/com/search/bus/bussearch/Surround_Fragment.java
+++ BusSearch/app/src/main/java/com/search/bus/bussearch/Surround_Fragment.java
@@ -2,25 +2,38 @@ package com.search.bus.bussearch;
 
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
 import com.amap.api.maps2d.AMap;
+import com.amap.api.maps2d.LocationSource;
 import com.amap.api.maps2d.MapView;
+import com.amap.api.maps2d.model.BitmapDescriptorFactory;
+import com.amap.api.maps2d.model.MyLocationStyle;
 
 /**
- * 作者 夏晔         修改者：李越
- *  2016/11/29.      2016.11.29
- *  编写             添加地图控件
+ * 作者 夏晔         修改者：李越        修改者：汪仑
+ *  2016/11/29.      2016.11.29          2016.11.30
+ *  编写             添加地图控件       添加定位功能
  */
-public class Surround_Fragment extends Fragment {
+public class Surround_Fragment extends Fragment implements LocationSource,
+        AMapLocationListener {
 
     private FragmentManager fm;
     private MapView mapView;
     private AMap aMap;
+    private LocationSource.OnLocationChangedListener mListener;
+    private AMapLocationClient mlocationClient;
+    private AMapLocationClientOption mLocationOption;
 
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -28,11 +41,45 @@ public class Surround_Fragment extends Fragment {
         View view=inflater.inflate(R.layout.surround_fragment, container, false);
         mapView = (MapView)view.findViewById(R.id.map);//获取控件
         mapView.onCreate(savedInstanceState);// 此方法必须重写
-        if (aMap == null) {
-            aMap = mapView.getMap();
-        }
+        init();
+
         return view;
     }
+
+    /**
+     * 作者：汪仑
+     * 2016.11.30
+     * 初始化AMap对象
+     */
+    private void init() {
+        if (aMap == null) {
+            aMap = mapView.getMap();
+            setUpMap();
+        }
+    }
+
+    /**
+     * 作者：汪仑
+     * 2016.11.30
+     * 设置一些amap的属性
+     */
+    private void setUpMap() {
+        // 自定义系统定位小蓝点
+        MyLocationStyle myLocationStyle = new MyLocationStyle();
+        myLocationStyle.myLocationIcon(BitmapDescriptorFactory
+                .fromResource(R.drawable.location_marker));// 设置小蓝点的图标
+        myLocationStyle.strokeColor(Color.BLACK);// 设置圆形的边框颜色
+        myLocationStyle.radiusFillColor(Color.argb(100, 0, 0, 180));// 设置圆形的填充颜色
+        // myLocationStyle.anchor(int,int)//设置小蓝点的锚点
+        myLocationStyle.strokeWidth(1.0f);// 设置圆形的边框粗细
+        aMap.setMyLocationStyle(myLocationStyle);
+        aMap.setLocationSource(this);// 设置定位监听
+        aMap.getUiSettings().setMyLocationButtonEnabled(true);// 设置默认定位按钮是否显示
+        aMap.setMyLocationEnabled(true);// 设置为true表示显示定位层并可触发定位，false表示隐藏定位层并不可触发定位，默认是false
+        // aMap.setMyLocationType()
+    }
+
+
 
     /**
      * 作者：李越
@@ -77,4 +124,70 @@ public class Surround_Fragment extends Fragment {
         super.onDestroy();
         mapView.onDestroy();
     }
+
+
+    /**
+     * 作者：汪仑
+     * 2016.11.30
+     * 定位成功后回调函数
+     */
+    public void onLocationChanged(AMapLocation amapLocation) {
+        if (mListener != null && amapLocation != null) {
+            if (amapLocation != null
+                    && amapLocation.getErrorCode() == 0) {
+                mListener.onLocationChanged(amapLocation);// 显示系统小蓝点
+            } else {
+                String errText = "定位失败," + amapLocation.getErrorCode()+ ": " + amapLocation.getErrorInfo();
+                Log.e("AmapErr",errText);
+            }
+        }
+    }
+
+    /**
+     * 作者：汪仑
+     * 2016.11.30
+     * 激活定位
+     */
+
+    public void activate(LocationSource.OnLocationChangedListener listener) {
+        mListener = listener;
+        if (mlocationClient == null) {
+            mlocationClient = new AMapLocationClient(getActivity());
+            mLocationOption = new AMapLocationClientOption();
+            //设置定位监听
+            mlocationClient.setLocationListener(this);
+            //设置为高精度定位模式
+            mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+            //设置定位参数
+            mlocationClient.setLocationOption(mLocationOption);
+            // 此方法为每隔固定时间会发起一次定位请求，为了减少电量消耗或网络流量消耗，
+            // 注意设置合适的定位时间的间隔（最小间隔支持为2000ms），并且在合适时间调用stopLocation()方法来取消定位请求
+            // 在定位结束后，在合适的生命周期调用onDestroy()方法
+            // 在单次定位情况下，定位无论成功与否，都无需调用stopLocation()方法移除请求，定位sdk内部会移除
+            mlocationClient.startLocation();
+        }
+    }
+
+    /**
+     * 作者：汪仑
+     * 2016.11.30
+     * 停止定位
+     */
+
+    public void deactivate() {
+        mListener = null;
+        if (mlocationClient != null) {
+            mlocationClient.stopLocation();
+            mlocationClient.onDestroy();
+        }
+        mlocationClient = null;
+    }
+
 }
+
+
+
+
+
+
+
