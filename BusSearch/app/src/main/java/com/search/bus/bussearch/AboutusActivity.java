7da2ede0937c0554ca42e26a75ab9abe27b52616
xiaye1 <1550469654@qq.com>
2016-12-05 20:23:18 +0800

    aboutus

---
+++ BusSearch/app/src/main/java/com/search/bus/bussearch/AboutusActivity.java
@@ -0,0 +1,15 @@
+package com.search.bus.bussearch;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by lenovo on 2016/12/5.
+ */
+public class AboutusActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.aboutus);
+    }
+}
