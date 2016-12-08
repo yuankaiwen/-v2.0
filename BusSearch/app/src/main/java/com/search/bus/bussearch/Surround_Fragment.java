80213ef4566c8b211c4ae005cdc4668281883907
xiaye1 <1550469654@qq.com>
2016-12-08 15:28:27 +0800

    添加覆盖物

--- BusSearch/app/src/main/java/com/search/bus/bussearch/Surround_Fragment.java
+++ BusSearch/app/src/main/java/com/search/bus/bussearch/Surround_Fragment.java
@@ -308,9 +308,9 @@ public class Surround_Fragment extends Fragment implements LocationSource,
          //夏晔        添加北京景点覆盖物    2016/12/8
          markerOption1x = new MarkerOptions().icon(BitmapDescriptorFactory
                 .defaultMarker(BitmapDescriptorFactory.HUE_RED))
-                .position(new LatLng(39.918058, 116.397026))
+                .position(new LatLng(39.91073, 116.394351))
                 .title("中山公园")
-                .snippet("详细信息")
+                .snippet("国家AAAA级旅游景区，占地23万平方米，是—座纪念性的古典坛庙园林。[1]  它原是明清两代的社稷坛，与太庙（今劳动人民文化宫）一起沿袭周代以来“左祖右社”的礼制建造。")
                 .draggable(true);
         marker1x = aMap.addMarker(markerOption1x);
         
