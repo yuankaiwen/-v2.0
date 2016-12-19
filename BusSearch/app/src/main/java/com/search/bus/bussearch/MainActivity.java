987415889ddb7563368cdc7db2c532d9057f6464
liyue1 <842414936@qq.com>
2016-12-19 15:56:22 +0800

    添加关于我们页面回退按钮

--- BusSearch/app/src/main/java/com/search/bus/bussearch/MainActivity.java
+++ BusSearch/app/src/main/java/com/search/bus/bussearch/MainActivity.java
@@ -3,6 +3,7 @@ package com.search.bus.bussearch;
 import android.app.Activity;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
@@ -94,6 +95,11 @@ public class MainActivity extends Activity {
         setListener();
         //3.设置默认的页面（fragment页面）
         setDefaultPage();
+        Intent intent=getIntent();
+        int Fid=intent.getIntExtra("Fid",-1);
+        if(Fid==1||Fid==2){
+            setChangePage();
+        }
     }
 
     //获取界面控件
@@ -128,5 +134,25 @@ public class MainActivity extends Activity {
         transaction.commit();
         btn1.setBackgroundDrawable(getResources().getDrawable(R.drawable.fragment_search_change));
         text1.setTextColor(Color.parseColor("#009FCC"));
+    }
+
+    /*作者：李越
+    *实现界面跳转之后的回退
+    * 2016.12.19
+     */
+    private void setChangePage() {
+        //1.获取一个FragmentManager对象
+        FragmentManager fm = getFragmentManager();
+        //2.获取FragmentTransaction对象
+        FragmentTransaction transaction = fm.beginTransaction();
+        mShezhi = new Setting_Fragment();
+        //3.设置页面
+        transaction.replace(R.id.fl, mShezhi);
+        //4.执行更改
+        transaction.commit();
+        btn3.setBackgroundDrawable(getResources().getDrawable(R.drawable.fragment_settings_change));
+        text3.setTextColor(Color.parseColor("#009FCC"));
+        btn1.setBackgroundDrawable(getResources().getDrawable(R.drawable.fragment_search));
+        text1.setTextColor(Color.parseColor("#898989"));
     }
 }
